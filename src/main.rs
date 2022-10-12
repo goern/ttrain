bc20447e3b23cfd5b60cc19909ff60f17e0aea6c
Christoph Görn <goern@b4mad.net>
2022-10-12 16:11:15 +0200

    add GraphQL schema
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+#[macro_use]
+extern crate juniper;
+
+use rusqlite::Connection;
+
 pub mod locations;
 pub mod vehicles;
 
+mod embedded {
+    use refinery::embed_migrations;
+    embed_migrations!("ttrain/migrations");
+}
+
 fn main() {
     stderrlog::new().module(module_path!()).init().unwrap();
+
+    let mut conn = Connection::open_in_memory().unwrap();
+    embedded::migrations::runner().run(&mut conn).unwrap();
 }
--- src/vehicles/land.rs
+++ src/vehicles/land.rs
@@ -1,6 +1,19 @@
+use juniper::FieldResult;
+
+#[derive(Debug, GraphQLObject)]
+#[graphql(description = "A car.")]
 pub struct Car {
     pub name: String,
     pub model: String,
     pub year: i32,
     pub color: String,
+}
+
+#[derive(GraphQLInputObject)]
+#[graphql(description = "A car.")]
+struct NewCar {
+    name: String,
+    model: String,
+    year: i32,
+    color: String,
 }
